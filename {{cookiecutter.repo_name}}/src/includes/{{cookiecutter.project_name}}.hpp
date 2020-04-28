37fb5cabccd42d5472cbabc330961245f082bff2
Eddy Ekofo <eddyecko@gmail.com>
2020-04-28 20:18:23 +0200

    Fix the .hpp file

--- {{cookiecutter.repo_name}}/src/includes/{{cookiecutter.project_name}}.hpp
+++ {{cookiecutter.repo_name}}/src/includes/{{cookiecutter.project_name}}.hpp
@@ -6,13 +6,3 @@ public:
     {{cookiecutter.project_name}}() = default;
     ~{{cookiecutter.project_name}}() = default;
 };
-#include "{{cookiecutter.project_name}}.hpp"
-
-class {{cookiecutter.project_name}} 
-{
-public:
-    {{cookiecutter.project_name}}() = default;
-    ~{{cookiecutter.project_name}}() = default;
-
-
-}

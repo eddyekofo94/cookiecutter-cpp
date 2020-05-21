793e9a995e7eae31f04fe95a28c9a00bef946b88
Eddy Ekofo <eddyecko@gmail.com>
2020-05-21 13:44:16 +0200

    Formatting

--- {{cookiecutter.repo_name}}/cmake_ru.sh
+++ {{cookiecutter.repo_name}}/cmake_ru.sh
@@ -17,56 +17,66 @@ SRC_BIN="{{ cookiecutter.project_name }}_src"
 TEST_BIN="{{ cookiecutter.project_name }}_test"
 
 build() {
-    if [[ -z $ARG_2 ]]; then
-        echo "Building ALL the targets"
-        cmake --build . -- -j4
+if [[ -z $ARG_2 ]]; then
+    echo "Building ALL the targets"
+    cmake --build . -- -j4
 
-    elif [[ "$ARG_2" == "--src" ]] || [[ "$ARG_2" == "-s" ]]; then
-        echo "Building \"$SRC_BIN\" executable target"
-        cmake --build . --target $SRC_BIN -- -j4
-    elif [[ "$ARG_2" == "--test" ]] || [[ "$ARG_2" == "-t" ]]; then
-        echo "Building \"$TEST_BIN\" executable target"
-        cmake --build . --target $TEST_BIN -- -j4
-    else
-        echo "Error: CMake target: \"$ARG_2\" is not found"
-    fi
+elif [[ "$ARG_2" == "--src" ]] || [[ "$ARG_2" == "-s" ]]; then
+    echo "Building \"$SRC_BIN\" executable target"
+    cmake --build . --target $SRC_BIN -- -j4
+elif [[ "$ARG_2" == "--test" ]] || [[ "$ARG_2" == "-t" ]]; then
+    echo "Building \"$TEST_BIN\" executable target"
+    cmake --build . --target $TEST_BIN -- -j4
+else
+    echo "Error: CMake target: \"$ARG_2\" is not found"
+fi
 }
 
 run_code() {
-    if [[ "$ARG_2" == "--src" ]] || [[ "$ARG_2" == "-s" ]]; then
-        ./bin/$SRC_BIN
-    elif [[ "$ARG_2" == "--test" ]] || [[ "$ARG_2" == "-t" ]]; then
-        ./bin/$TEST_BIN
-    else
-        echo "Error: connot run target \"$ARG_2\""
+if [[ "$ARG_2" == "--src" ]] || [[ "$ARG_2" == "-s" ]]
+then
+    if [[ ! -f $SRC_BIN ]];
+    then
+        build $ARG_2
     fi
+    ./bin/$SRC_BIN
+elif [[ "$ARG_2" == "--test" ]] || [[ "$ARG_2" == "-t" ]]
+then
+    if [[ ! -f $TEST_BIN ]];
+    then
+        build $ARG_2
+    fi
+    ./bin/$TEST_BIN
+else
+    echo "Error: connot run target \"$ARG_2\""
+fi
 }
 
 clean() {
-    if [[ -z $ARG_2 ]]; then
-        cmake --clean-first ../CMakeLists.txt -B $BUILD_DIR
-    elif [[ "$ARG_2" == "--src" ]] || [[ "$ARG_2" == "-s" ]]; then
-        cmake --build . --clean-first --target $SRC_BIN -- -j4
-    elif [[ "$ARG_2" == "--test" ]] || [[ "$ARG_2" == "-t" ]]; then
-        cmake --build . --clean-first --target $TEST_BIN -- -j4
-    fi
+if [[ -z $ARG_2 ]]; then
+    cmake --clean-first ../CMakeLists.txt -B $BUILD_DIR
+elif [[ "$ARG_2" == "--src" ]] || [[ "$ARG_2" == "-s" ]]; then
+    cmake --build . --clean-first --target $SRC_BIN -- -j4
+elif [[ "$ARG_2" == "--test" ]] || [[ "$ARG_2" == "-t" ]]; then
+    cmake --build . --clean-first --target $TEST_BIN -- -j4
+fi
 }
 
 cmake_run() {
-    if [[ $# -eq 0 ]]; then
-        echo "Building ALL the targets"
-        cmake --build . -- -j4
+if [[ $# -eq 0 ]]; then
+    echo "Building ALL the targets"
+    cmake --build . -- -j4
+else
+    if [[ "$ARG_1" == "--build" ]] || [[ "$ARG_1" == "-b" ]]; then
+        build $ARG_1
+    elif [ "$ARG_1" == "--run" ] || [ "$ARG_1" == "-r" ]; then
+        run_code $ARG_1
+    elif [[ "$ARG_1" == "--clean" ]] || [[ "$ARG_1" == "-c" ]]; then
+        clean $ARG_1
     else
-        if [[ "$ARG_1" == "--build" ]] || [[ "$ARG_1" == "-b" ]]; then
-            build $ARG_1
-        elif [ "$ARG_1" == "--run" ] || [ "$ARG_1" == "-r" ]; then
-            run_code $ARG_1
-        elif [[ "$ARG_1" == "--clean" ]] || [[ "$ARG_1" == "-c" ]]; then
-            clean $ARG_1
-        else
-            echo "Error: \"$ARG_1\" argument is not found"
-        fi
+        echo "Error: \"$ARG_1\" argument is not found"
     fi
+fi
 }
 
 if [[ $(pwd) != $PROJECT_ROOT_DIR ]]; then
